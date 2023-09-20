2f17f5bf34ddeda951528b192d64f61e035fafa0
Nick Fisher <nick.fisher@polyvox.app>
2023-09-20 17:10:16 +0800

    fix buffer clipping

--- lib/flutter_sherpa_onnx_ffi_isolate.dart
+++ lib/flutter_sherpa_onnx_ffi_isolate.dart
@@ -195,17 +195,6 @@ class FlutterSherpaOnnxFFIIsolateRunner {
     _lib.AcceptWaveform(
         _stream!, _sampleRate!, floatPtr, _chunkLengthInSamples);
 
-    var dir = await getExternalStorageDirectories();
-    var outfile = File(dir![0].path + "/tmp.pcm");
-
-    var bd = ByteData.sublistView(floatPtr.asTypedList(_chunkLengthInSamples));
-    var uint8list = Uint8List(bd.lengthInBytes);
-    for (int i = 0; i < bd.lengthInBytes; i++) {
-      uint8list[i] = bd.getUint8(i);
-    }
-
-    outfile.writeAsBytesSync(uint8list, mode: FileMode.append);
-
     while (_lib.IsOnlineStreamReady(_recognizer!, _stream!) == 1) {
       _lib.DecodeOnlineStream(_recognizer!, _stream!);
     }
