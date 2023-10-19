d1ff90d28c02ce035ae572b5985af1ba3243e1db
Nick Fisher <nick.fisher@polyvox.app>
2023-10-19 22:25:41 +0800

    remove unnecessary plugin files

--- lib/flutter_sherpa_onnx.dart
+++
@@ -1,8 +0,0 @@
-
-import 'flutter_sherpa_onnx_platform_interface.dart';
-
-class FlutterSherpaOnnx {
-  Future<String?> getPlatformVersion() {
-    return FlutterSherpaOnnxPlatform.instance.getPlatformVersion();
-  }
-}
